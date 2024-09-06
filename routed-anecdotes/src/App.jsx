ae1807d82089cf4622aeaa86b6b3359055d587a9
vikram(wsl) <vikramjit360@gmail.com>
2024-09-06 20:10:35 +0530

    7.6 - fix: fix the invalid prop being passed to input tag in CreateNew

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -78,9 +78,9 @@ const Footer = () => (
 );
 
 const CreateNew = ({ addNew }) => {
-  const content = useField("content");
-  const author = useField("author");
-  const info = useField("info");
+  const { reset: resetContent, ...content } = useField("content");
+  const { reset: resetAuthor, ...author } = useField("author");
+  const { reset: resetInfo, ...info } = useField("info");
   const navigate = useNavigate();
 
   const handleSubmit = e => {
@@ -95,9 +95,9 @@ const CreateNew = ({ addNew }) => {
   };
 
   const handleResetFields = () => {
-    content.reset();
-    author.reset();
-    info.reset();
+    resetContent();
+    resetAuthor();
+    resetInfo();
   };
 
   return (
