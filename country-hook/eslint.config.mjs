0d7d9213d65f526bb74e00e9e385730ecc3eac8a
vikram(wsl) <vikramjit360@gmail.com>
2024-09-06 20:21:30 +0530

    clone git@github.com:fullstack-hy2020/country-hook.git to use as base for country-hook

---
+++ country-hook/eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "no-unused-vars": 1,
+      "react/prop-types": 0,
+    },
+  },
+];
